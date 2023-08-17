94458062da6e4fec5b5297d23d9d6c3d83fdb679
Altamir <box.altamir@gmail.com>
2023-08-17 17:00:18 +0100

    Correção de bugs com o triãngulo Isóceles.

--- src/Triangulos.java
+++ src/Triangulos.java
@@ -1,16 +1,22 @@
+import calculos.DefinicaoTriangulo;
+
 import java.util.Scanner;
 
 public class Triangulos {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        DefinicaoTriangulo defTriangulo = new DefinicaoTriangulo();
+
         System.out.println("tipos de Triangulo:");
-        System.out.println("Insira os Lados do Triângulo , assim verificaremos a área e qual o tipo do triângulo:");
+        System.out.println("Insira os Lados do Triângulo , assim verificaremos o perímetro e qual o tipo do triângulo:");
         System.out.println("Lado 01");
-        int ladoA  = sc.nextInt();
+        defTriangulo.aSide = sc.nextInt();
         System.out.println("Lado 02");
-        int ladoB  = sc.nextInt();
+        defTriangulo.bSide = sc.nextInt();
         System.out.println("Lado 03");
-        int ladoC  = sc.nextInt();
+        defTriangulo.cSide = sc.nextInt();
+
+        defTriangulo.calc();
 
         sc.close();
     }
