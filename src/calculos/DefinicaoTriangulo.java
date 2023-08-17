e53fe2bda73cbf6816c660f6ed9c659c0c8d470a
Altamir <box.altamir@gmail.com>
2023-08-17 17:20:26 +0100

    Higienização do código. Parte Final.

--- src/calculos/DefinicaoTriangulo.java
+++ src/calculos/DefinicaoTriangulo.java
@@ -8,17 +8,18 @@ public class DefinicaoTriangulo {
     public int cSide;
     public String name;
     public int perimeter;
-    public void calc(){
-        if(aSide == bSide && bSide == cSide){
-            perimeter = aSide+bSide+cSide;
-            System.out.printf("Triângulo Equilátero com perímetro %d%n",perimeter);
+
+
+    public void calc() {
+        if (aSide == bSide && bSide == cSide) {
+            perimeter = aSide + bSide + cSide;
+            System.out.printf("Triângulo Equilátero com perímetro %d%n", perimeter);
         } else if (aSide == bSide || aSide == cSide || bSide == cSide) {
-            perimeter = aSide+bSide+cSide;
-            System.out.printf("Triângulo Isóceles com perímetro %d%n",perimeter);
-            
-        }else{
-            perimeter = aSide+bSide+cSide;
-            System.out.printf("Triângulo Escaleno com perímetro %d%n",perimeter);
+            perimeter = aSide + bSide + cSide;
+            System.out.printf("Triângulo Isóceles com perímetro %d%n", perimeter);
+        } else {
+            perimeter = aSide + bSide + cSide;
+            System.out.printf("Triângulo Escaleno com perímetro %d%n", perimeter);
         }
 
     }
