041aa7488f111af9d05b03e92d32f7166a7697cb
Altamir <box.altamir@gmail.com>
2023-08-17 15:33:12 +0100

    Base para definição dos triângulos.

--- src/Triangulos.java
+++ src/Triangulos.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Triangulos {
+import java.util.Scanner;
+
+public class Triangulos {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("tipos de Triangulo:");
+        System.out.println("Insira os Lados do Triângulo , assim verificaremos a área e qual o tipo do triângulo:");
+        System.out.println("Lado 01");
+        int ladoA  = sc.nextInt();
+        System.out.println("Lado 02");
+        int ladoB  = sc.nextInt();
+        System.out.println("Lado 03");
+        int ladoC  = sc.nextInt();
+
+        sc.close();
+    }
 }
