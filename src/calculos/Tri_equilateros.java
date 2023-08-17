174ae8d77d53dd826ced7f854c1aee2271c0a50e
Altamir <box.altamir@gmail.com>
2023-08-17 15:33:12 +0100

    Base para definição dos triângulos.

---
+++ src/calculos/Tri_equilateros.java
@@ -0,0 +1,16 @@
+package calculos;
+
+public class Tri_equilateros {
+    private int aSide;
+    private int bSide;
+    private int cSide;
+    private int result;
+
+    public int  area(int aSide, int bSide, int cSide) {
+        this.aSide = aSide;
+        this.bSide = bSide;
+        this.cSide = cSide;
+        return  result;
+    }
+}
+
